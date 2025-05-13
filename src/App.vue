24110a091622c131845318596218e09771dde1ea
Pilarsen <pilarsen@studio-aw.ru>
2025-05-13 21:02:58 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Hero title="Летние каникулы с IT обучением" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Hero from './components/Hero.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Hero,
+  },
+});
+</script>
---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero">
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Hero',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
